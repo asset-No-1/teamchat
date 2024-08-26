669c31b184563e59f83f4df15f98af8067d0b887
sunwoo <summerham22@gmail.com>
2024-08-26 11:35:16 +0900

    fix call

--- src/teamchat/call.py
+++ src/teamchat/call.py
@@ -2,42 +2,36 @@ import requests
 import os
 import json
 
-def req(year=2022):
-    url = gen_url(year)
+def req(url):
     r = requests.get(url)
-    code = r.status_code
-    data = r.json()
-    
-    return code, data
+    j = r.json()
 
-def gen_url(year=2022):
-    base_url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json"
-    key = get_key()
-    url = f"{base_url}?key={key}&openStartDt={year}&openEndDt={year}"
+    return j
 
-    return url
-
-def get_key():
-    key = os.getenv('MOVIE_API_KEY')
-    return key
-
-def req2list(year=2022):
-    _, data = req(year)
-    l = data['movieListResult']['movieList']
-    return l
-
-def list2df(year=2022):
-    l = req2list(year)
-
-    return l
-
-def save_json(data, file_path):
-    data = list2df(year=year)
+def save_movies(year, sleep_time=1):
+    #연도별 저장
     home_path = os.path.expanduser("~")
     file_path = f"{home_path}/data/mov_data/year={year}/data.json"
+
+    #위 경로가 있으면 API 호출을 멈추고 프로그램 종료
+    if os.path.exists(file_path):
+        print(f"파일이 이미 존재합니다. (연도: {year})")
+        return []
+    else:
+        print(f"데이터를 저장합니다. (연도: {year})")
+
+    API_KEY = os.getenv('MOVIE_API_KEY')
+    url_base = f"https://kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key={API_KEY}&openStartDt={year}&openEndDt={year}"
+    r = req(url_base)
+    data = r['movieListResult']['movieList']
+    return data
+
+def save_json(data, file_path):
+    #파일저장 경로 mkdir
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
 
     with open(file_path, 'w', encoding='utf-8') as f:
         json.dump(data, f, indent=4, ensure_ascii=False)
-    pass
+
+    return True
 
--- tests/test_call.py
+++ tests/test_call.py
@@ -1,15 +1,7 @@
-from teamchat.call import save_json, list2df
+from teamchat.call import save_movies
 import os
 
-#def test_save2df():
-#    r = save2df('20220101')
-#
-#    assert r
-
 def test_save_json():
-    data = list2df(year=2022)
-    home_path = os.path.expanduser("~")
-    file_path = f"{home_path}/data/mov_data/year=2022/data.json"
-    r = save_json(data, file_path)
+    r = save_movies(2023)
 
-    assert True
+    assert r
