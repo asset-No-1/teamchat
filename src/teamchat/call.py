93da6177c953e851531263158e5a051856223fca
sunwoo <summerham22@gmail.com>
2024-08-26 10:46:27 +0900

    fix save_json

--- src/teamchat/call.py
+++ src/teamchat/call.py
@@ -2,7 +2,7 @@ import requests
 import os
 import json
 
-def req(year='2022'):
+def req(year=2022):
     url = gen_url(year)
     r = requests.get(url)
     code = r.status_code
@@ -10,7 +10,7 @@ def req(year='2022'):
     
     return code, data
 
-def gen_url(year="2022"):
+def gen_url(year=2022):
     base_url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json"
     key = get_key()
     url = f"{base_url}?key={key}&openStartDt={year}&openEndDt={year}"
@@ -21,21 +21,22 @@ def get_key():
     key = os.getenv('MOVIE_API_KEY')
     return key
 
-def req2list(year='2022'):
+def req2list(year=2022):
     _, data = req(year)
     l = data['movieListResult']['movieList']
     return l
 
-def list2df(year='2022'):
+def list2df(year=2022):
     l = req2list(year)
 
     return l
 
 def save_json(data, file_path):
-    data = list2df(year='2022')
+    data = list2df(year)
     home_path = os.path.expanduser("~")
     file_path = f"{home_path}/data/mov_data/year={year}/data.json"
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
     with open(file_path, 'w', encoding='utf-8') as f:
         json.dump(data, f, indent=4, ensure_ascii=False)
     pass
--- tests/test_call.py
+++ tests/test_call.py
@@ -1,4 +1,5 @@
-from teamchat.api.call import save_json, list2df
+from teamchat.call import save_json, list2df
+import os
 
 #def test_save2df():
 #    r = save2df('20220101')
@@ -6,7 +7,9 @@ from teamchat.api.call import save_json, list2df
 #    assert r
 
 def test_save_json():
-    data = list2df(year='2022')
-    r = save_json(data)
+    data = list2df(year=2022)
+    home_path = os.path.expanduser("~")
+    file_path = f"{home_path}/data/mov_data/year=2022/data.json"
+    r = save_json(data, file_path)
 
     assert True
