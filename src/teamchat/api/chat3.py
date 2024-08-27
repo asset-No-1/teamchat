f98c73d409953c57205ac6b5553bf1c556c6f57a
sunwoo <summerham22@gmail.com>
2024-08-27 15:21:56 +0900

    exit bug fix

--- src/teamchat/api/chat3.py
+++ src/teamchat/api/chat3.py
@@ -3,26 +3,32 @@ from json import loads, dumps, load, JSONDecodeError
 import threading
 import os
 
+
 #PRODUCER 설정
 def pro_chat(username):
     producer = KafkaProducer(
         bootstrap_servers=['ec2-43-203-210-250.ap-northeast-2.compute.amazonaws.com:9092'],
         value_serializer=lambda x: dumps(x, ensure_ascii=False).encode('utf-8'),
     )
+    
 
     while True:
-        data = input()
-    
+        data = input(f'{username}: ')
+        
         if data == 'exit':
+            # exit 메시지를 전송하여 컨슈머에게 종료 신호를 보냄
+            message = {'user': username, 'message': 'exit'}
+            producer.send('chat', value=message)
+            producer.flush()
             break
-    
+
         else:
             # 메시지를 JSON으로 보내면서 사용자 이름을 포함시킵니다.
             message = {'user': username, 'message': data}
             producer.send('chat', value=message)
             producer.flush()
-
-    print("챗봇 종료")
+    
+    print("채팅 종료")
 
 
 #CONSUMER 설정
@@ -37,15 +43,19 @@ def con_chat(username):
     try:
         for m in consumer:
             message = m.value
-            # 수신한 메시지에서 사용자 이름과 메시지를 출력합니다.
-            print(f"{message['user']}: {message['message']}")
 
-            #if message['user'] == username:
+            if message['message'] == 'exit':
+                break
+
+            # 수신한 메시지에서 사용자 이름과 메시지를 출력합니다.
+            if message['user'] != username:
+                print(f"{message['user']}: {message['message']}")
+
+            #if message['user'] != username:
+            #  print(f"{message['user']}: {message['message']}")
              #   print(f"나: {message['message']}\n")
             
-            #else:
-            #    print("몰라")
-
+            
     except KeyboardInterrupt:
         print("Consumer 종료")
 
@@ -57,6 +67,8 @@ def con_chat(username):
         print(f"TypeError: {e}")
         print(f"Raw message: {message}")
 
+    finally:
+        consumer.close()
 
 # 각 사용자 이름을 입력받아 PRODUCER와 CONSUMER 실행
 username = input("이름: ")
